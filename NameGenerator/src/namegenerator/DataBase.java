18eb02355155813294c0de67d8aa5bf6c48b66cb
daniel_gurbiel <daniel.gurbiel@gmail.com>
2018-03-01 23:18:06 +0100

    1.0

--- NameGenerator/src/namegenerator/DataBase.java
+++ NameGenerator/src/namegenerator/DataBase.java
@@ -1,8 +1,60 @@
+
 package namegenerator;
-
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
 public class DataBase {
-
-    public DataBase() {
-    }
     
+    String URLConnection;
+    static String dataFromBase;
+    String query;
+    Connection connection;
+    Statement stmt;
+    ResultSet rs;
+    
+    public DataBase(String query){
+        URLConnection = "jdbc:mysql://localhost:3306/name_generator";
+        this.query = query;
+        connection = null;
+
+        try {
+            connection = DriverManager.getConnection(URLConnection, "root", "");
+            Class.forName("com.mysql.jdbc.Driver");
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+       
+                        while (rs.next()) {
+                                showDataFromBase(rs);
+                        }
+                        connection.close();
+                }
+
+                catch(ClassNotFoundException exception) {
+                        System.out.println("Error driver!");
+                }
+ 
+                catch(SQLException exception) {
+                    System.out.println("SQLException: " + exception.getMessage());
+                    System.out.println("SQLState: " + exception.getSQLState());
+                    System.out.println("VendorError: " + exception.getErrorCode());
+                }
+        
+    }
+ 
+        
+        static void showDataFromBase(ResultSet rs){
+                try{
+                dataFromBase = rs.getString("name");
+                System.out.println(dataFromBase);
+               /* dataFromBase = rs.getString(2);
+                System.out.println(dataFromBase + " ");
+                dataFromBase = rs.getString(3);
+                System.out.println(dataFromBase);*/
+                }catch(SQLException e) {
+                        e.printStackTrace();
+                }
+        }
 }
+              
