ec2db0fe1965ef390c5c7fc12ec591e7376f9149
daniel_gurbiel <daniel.gurbiel@gmail.com>
2018-03-01 10:56:44 +0100

    1.0

---
+++ NameGenerator/src/namegenerator/NameGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package namegenerator;
+
+/**
+ *
+ * @author Daniel
+ */
+public class NameGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
